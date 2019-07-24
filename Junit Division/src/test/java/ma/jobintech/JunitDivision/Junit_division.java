42d781481c272e329188d7de3386d993f54189fd
Ismail NORRI <ismail.norri.24@gmail.com>
2019-07-24 09:29:15 +0100

    new

--- Junit Division/src/test/java/ma/jobintech/JunitDivision/Junit_division.java
+++ Junit Division/src/test/java/ma/jobintech/JunitDivision/Junit_division.java
@@ -9,21 +9,18 @@ import serviceCalculatrice.Calculatrice;
 
 class Junit_division {
 	
-	@BeforeAll
-	public static void initialiser() {
-		Calculatrice calcDivision = new Calculatrice();
-	}
 	
 	
 	@Test
 	void Division() {
 //		fail("La méthode Division n'est pas encore implémentée");
+		Calculatrice calcDivision = new Calculatrice();
 		
 		int diviseur = 0 , divise = 2 , resultDivReel , resultDivAttendu = 2;
 		resultDivReel = divise/diviseur;
 		resultDivReel = calcDivision.Diviser(divise, diviseur);	
 		
-		assertEquals(resultDivAttendu, resultDivReel);
+		assertEquals(resultDivReel,ArithmeticException);
 		
 	}
 	
